607e33982d0f1eac0446aa9786d34ab6655c6384
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-06 02:10:29 +0900

    UPDATE detr's last padding classification noun

--- models/detr.py
+++ models/detr.py
@@ -34,7 +34,7 @@ class DETR(nn.Module):
         self.num_queries = num_queries
         self.transformer = transformer
         hidden_dim = transformer.d_model
-        self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
+        self.class_embed = nn.Linear(hidden_dim, num_classes)
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
