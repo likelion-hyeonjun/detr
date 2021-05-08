67643deaf3d3e76e9bc2073e5dd75122a62904ba
likelion-hyeonjun <snail1882@naver.com>
2021-05-09 05:56:42 +0900

    make verb_emb 1 w/o and w role

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .detr import build
+from .detr_verb_emb_1_w_role import build
 
-
+#change .py file to use appropriate transformer model.
 def build_model(args):
     return build(args)
