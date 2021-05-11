8e890d8b6bd5ef970eae4ae649f3bfe52eea2e52
likelion-hyeonjun <snail1882@naver.com>
2021-05-12 03:25:45 +0900

    Fix bug (loss_nce)

--- models/detr.py
+++ models/detr.py
@@ -418,7 +418,7 @@ def build(args):
     )
     if args.masks:
         model = DETRsegm(model, freeze_detr=(args.frozen_weights is not None))
-    weight_dict = {'loss_ce': 1, 'loss_bbox': args.bbox_loss_coef}
+    weight_dict = {'loss_nce': 1, 'loss_bbox': args.bbox_loss_coef}
     weight_dict['loss_vce'] = args.loss_ratio
     weight_dict['loss_giou'] = args.giou_loss_coef
     if args.masks:
