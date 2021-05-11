803ad8c3137bc4186bffd80c45706971cddf7795
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-12 04:19:58 +0900

    UPDATE verb_info init

--- datasets/imsitu.py
+++ datasets/imsitu.py
@@ -42,7 +42,6 @@ class CSVDataset(Dataset):
         self.class_list = class_list
         self.verb_path = verb_path
         self.role_path = role_path
-        self.verb_info = verb_info
         self.transform = transform
         self.is_visualizing = is_visualizing
 
@@ -74,6 +73,9 @@ class CSVDataset(Dataset):
         for image_name in self.image_names:
             self.image_to_image_idx[image_name] = i
             i += 1
+
+        # verb_role
+        self.verb_role = {verb: value['order'] for verb, value in verb_info.items()}
 
     def load_classes(self, csv_reader):
         result = {}
@@ -144,8 +146,7 @@ class CSVDataset(Dataset):
         verb = verb.split('_')[0]
 
         verb_idx = self.verb_to_idx[verb]
-        verb_role = self.verb_info[verb]['order']
-        verb_role_idx = [self.role_to_idx[role] for role in verb_role]
+        verb_role_idx = [self.role_to_idx[role] for role in self.verb_role[verb]]
         sample = {'img': img, 'annot': annot,
                   'img_name': self.image_names[idx], 'verb_idx': verb_idx, 'verb_role_idx': verb_role_idx}
         if self.transform:
