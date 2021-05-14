0a5ec3f2ab2190c48c9700b23b53f0622ecb143d
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-14 15:20:27 +0000

    FIX type to action

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def get_args_parser():
                         help="Number of verb embed to cat role query slots")
     parser.add_argument('--num_role_queries', type=int, choices=[190],
                         help="Number of role query slots")
-    parser.add_argument('--gt_role_queries', type="store_true",
+    parser.add_argument('--gt_role_queries', action="store_true",
                         help="Select gt role queries")
     parser.add_argument('--pre_norm', action='store_true')
 
