956e6bb7b17f68728f038b8d46c7409402164927
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-14 15:53:06 +0000

    FIX target str in training

--- engine.py
+++ engine.py
@@ -27,7 +27,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         samples = samples.to(device)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        targets = [{k: v.to(device) if type(k) is not str else v for k, v in t.items()} for t in targets]
 
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
