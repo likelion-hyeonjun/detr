1c042c0de7ca632a84544b225355454dcfeefd4b
YoNyeoSeok <33247600+YoNyeoSeok@users.noreply.github.com>
2021-05-18 04:22:38 +0900

    Delete train.sh

--- train.sh
+++
@@ -1,1 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py  --batch_size 128 --use_verb_decoder --num_verb_embed 504 --num_role_queries 190 --gt_role_queries --output_dir "verb_dual_w_decoder/" | tee verb_dual_w_decoder.log
