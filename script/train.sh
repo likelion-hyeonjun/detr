cdfafcaec044d7d221ea0d0e2b6a79ed7237ebd7
jhcho <cjh990910@gmail.com>
2021-05-22 14:16:16 +0900

    UPDATE Adamax script

--- script/train.sh
+++ script/train.sh
@@ -31,3 +31,18 @@ git checkout bottomup
 #     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
 #     tee -a log/${NAME}.log
 
+# cluster 3 screen Adamax
+# train_bottomup
+# optimizer adamax
+# original image
+NAME="bottomup_adamax"
+OPTIM="Adamax"
+rm -f log/${NAME}.log dist/${NAME}
+rm -rf log/${NAME}
+
+CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch --nproc_per_node=4 --use_env \
+    main.py --batch_size 32 --epochs 50 --optimizer ${OPTIM} \
+    --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
+    while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
+    tee -a log/${NAME}.log
+
