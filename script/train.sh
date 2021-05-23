8ac0e85003106853dc150aa8f05974543cb9a73b
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-23 16:09:20 +0900

    UPDATE train script

--- script/train.sh
+++ script/train.sh
@@ -1,124 +1,23 @@
-git checkout bottomup
+git checkout parallel_srtr
 
-# cluster 17 screen Adam
-# train_bottomup
-# optimizer adam
-# original image
-# NAME="bottomup_adam"
-# OPTIM="Adam"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 64 --epochs 50 --optimizer ${OPTIM} \
-#     --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
-
-
-# cluster 17 screen AdamW
-# train_bottomup
-# optimizer adamw
-# original image
-# NAME="bottomup_adamw"
-# OPTIM="AdamW"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 64 --epochs 50 --optimizer ${OPTIM} \
-#     --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
-
-# cluster 3 screen Adamax
-# train_bottomup
-# optimizer adamax
-# original image
-# NAME="bottomup_adamax"
-# OPTIM="Adamax"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch --nproc_per_node=4 --use_env \
-#     main.py --batch_size 32 --epochs 50 --optimizer ${OPTIM} \
-#     --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
-
-# cluster 5 screen SGD
-# train_bottomup
-# optimizer sgd
-# original image
-# NAME="bottomup_sgd"
-# OPTIM="SGD"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="1,2,3" python -m torch.distributed.launch --nproc_per_node=3 --use_env \
-#     main.py --batch_size 32 --epochs 50 --optimizer ${OPTIM} \
-#     --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
-
-# cluster 141 screen RMSprop
-# train_bottomup
-# optimizer rmsprop
-# original image
-# NAME="bottomup_rmsprop"
-# OPTIM="RMSprop"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 64 --epochs 50 --optimizer ${OPTIM} \
-#     --output_dir log/${NAME} --dist_url file://${PWD}/dist/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
-
-# cluster 17 check_images
-# NAME="bottomup_check_images"
-# OPTIM="Adam"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 64 --epochs 1 --optimizer ${OPTIM}
-
-# cluster 17 resize 512
-# NAME="bottomup_resize_512"
-# OPTIM="Adam"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 16 --epochs 50 --optimizer ${OPTIM} \
-#     --dist_url file://${PWD}/dist/${NAME}
-
-# cluster 17 remove_crop
-# NAME="bottomup_remove_crop"
-# OPTIM="Adam"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 32 --epochs 50 --optimizer ${OPTIM} \
-#     --dist_url file://${PWD}/dist/${NAME}
-
-
-# cluster 17 compare_crop_512
-# NAME="bottomup_remove_crop"
-# OPTIM="AdamW"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 16 --epochs 50 --backbone resnet50 --optimizer ${OPTIM} --lr 1e-4 --lr_backbone 1e-5 --remove_crop \
-#     --dist_url file://${PWD}/dist/${NAME}
-
-NAME="bottomup_keep_crop"
-OPTIM="AdamW"
+# sandia (RTX3090 24GB x1)
+NAME="base"
 rm -f log/${NAME}.log dist/${NAME}
 rm -rf log/${NAME}
-CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-    main.py --batch_size 16 --epochs 50 --backbone resnet50 --optimizer ${OPTIM} --lr 1e-4 --lr_backbone 1e-5 \
-    --dist_url file://${PWD}/dist/${NAME}
+
+CUDA_VISIBLE_DEVICES="0" python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py \
+    --backbone resnet50 \
+    --batch_size 32 \
+    --optimizer Adam \
+    --lr 1e-4 \
+    --lr_backbone 1e-5 \
+    --weight_decay 0.0 \
+    --clip_max_norm 0.1 \
+    --verb_loss_coef 1 \
+    --noun_loss_coef 1 \
+    --epochs 50 \
+    --num_workers 4 \
+    --dist_url file://${PWD}/dist/${NAME} \
+    --output_dir log/${NAME} \
+    | while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done \
+    | tee -a log/${NAME}.log
