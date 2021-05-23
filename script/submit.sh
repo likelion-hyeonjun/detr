e2a04b361bb94faa13348e52bde09bdb56e356d2
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-23 16:39:16 +0900

    UPDATE submit script

---
+++ script/submit.sh
@@ -0,0 +1,35 @@
+git checkout parallel_srtr
+
+# titanxp ()
+NAME="base"
+rm -rf log/"titanxp_${NAME}"
+python run_with_submitit.py \
+    --backbone resnet50 \
+    --partition titanxp --batch_size 16 \
+    --optimizer Adam \
+    --lr 1e-4 \
+    --lr_backbone 1e-5 \
+    --weight_decay 0.0 \
+    --clip_max_norm 0.1 \
+    --verb_loss_coef 1 \
+    --noun_loss_coef 1 \
+    --epochs 15 \
+    --num_workers 4 \
+    --job_dir log/"titanxp_${NAME}"
+
+# 2080ti 8
+NAME="base"
+rm -rf log/"2080ti_${NAME}"
+python run_with_submitit.py \
+    --backbone resnet50 \
+    --partition 2080ti --batch_size 8 \
+    --optimizer Adam \
+    --lr 1e-4 \
+    --lr_backbone 1e-5 \
+    --weight_decay 0.0 \
+    --clip_max_norm 0.1 \
+    --verb_loss_coef 1 \
+    --noun_loss_coef 1 \
+    --epochs 15 \
+    --num_workers 4 \
+    --job_dir log/"2080ti_${NAME}"
